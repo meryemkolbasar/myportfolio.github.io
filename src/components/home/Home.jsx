3c3946c9a59046722b141f962583562410596692
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2023-11-09 16:59:08 +0100

    v

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import "./home.css";
+import Me from "../../assets/avatar-1.svg"
 
 const Home = () => {
     return (
-        <div>Home</div>
+       <section className="home conatiner" id="home">
+        <div className="intro"></div>
+        <img src="" alt="" className="home__img" />
+        <h1 className="home__name">Meryem Kolbasar</h1>
+        <span className="home__education">I'm a Full-Stack Java developer</span>
+
+
+
+       </section>
     )
 }
 
