1d53c4adc74ded1a40c07810ac21b1a1390e3c7e
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2023-11-14 16:21:56 +0100

    -v1 blog

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./blog.css";
+import Image1 from "../../assets/blog-1.svg";
+import Image2 from "../../assets/blog-2.svg";
+import Image3 from "../../assets/blog-3.svg";
+
+
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className="blog container section" id="blog">
+      <h2 className="section__title">Latest Posts</h2>
+
+      <div className="blog__container grid">
+        <div className="blog__card">
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
