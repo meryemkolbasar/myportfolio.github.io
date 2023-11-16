75126d0df0ada2a34fd41c3ca9243c2ba4315930
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2023-11-16 17:02:32 +0100

    -u

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import "./sidebar.css";
 import Logo from "../../assets/logo.svg"
+
 const Sidebar = () => {
     const [toggle, showMenu] = useState(false);
+
+   
     return (
        <>
        <aside className={toggle ? "aside show-menu" : "aside"}>
@@ -10,9 +13,11 @@ const Sidebar = () => {
             <img src={Logo}  alt="" />
         </a>
 
+        
       <nav className="nav">
         <div className="nav__menu">
             <ul className="nav__list">
+                
                 <li className="nav__item">
                     <a href="#home" className="nav__link">
                         <i className="icon-home"></i>
