52fb806d062fe9e11f4ab2c6f6408e7960df9974
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2024-07-11 22:13:17 +0200

    -v

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import "./sidebar.css";
-import Logo from "../../assets/logo.svg"
+import Logo from "../../assets/favicon.ico"
 
 const Sidebar = () => {
     const [toggle, showMenu] = useState(false);
