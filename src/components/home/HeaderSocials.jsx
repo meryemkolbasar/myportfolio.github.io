da802bdf03fa235f67c830626223924552dbed4d
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2024-08-18 15:19:33 +0200

    -v

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -1,30 +1,55 @@
-import React from 'react'
+import React from 'react';
 
 const HeaderSocials = () => {
   return (
     <div className="home__socials">  
-        <a href="https://www.instagram.com/devmeryemkolbasar/" className="home__social-link" target="_blank">
-        <i class="fa-brands fa-instagram"></i>
-        </a>
+      <a 
+        href="https://www.instagram.com/devmeryemkolbasar/" 
+        className="home__social-link" 
+        target="_blank" 
+        rel="noreferrer"
+      >
+        <i className="fa-brands fa-instagram"></i>
+      </a>
 
-        <a href="https://www.linkedin.com/in/meryemkolbasar/" className="home__social-link" target="_blank">
-        <i class="fa-brands fa-linkedin"></i>
-        </a>
+      <a 
+        href="https://www.linkedin.com/in/meryemkolbasar/" 
+        className="home__social-link" 
+        target="_blank" 
+        rel="noreferrer"
+      >
+        <i className="fa-brands fa-linkedin"></i>
+      </a>
 
-        <a href="https://github.com/meryemkolbasar" className="home__social-link" target="_blank">
-        <i class="fa-brands fa-github"></i>
-        </a>
-        
-        <a href="https://www.xing.com/profile/Meryem_Kolbasar/web_profiles" className="home__social-link" target="_blank">
-        <i class="fa-brands fa-xing"></i>
-        </a>
+      <a 
+        href="https://github.com/meryemkolbasar" 
+        className="home__social-link" 
+        target="_blank" 
+        rel="noreferrer"
+      >
+        <i className="fa-brands fa-github"></i>
+      </a>
+      
+      <a 
+        href="https://www.xing.com/profile/Meryem_Kolbasar/web_profiles" 
+        className="home__social-link" 
+        target="_blank" 
+        rel="noreferrer"
+      >
+        <i className="fa-brands fa-xing"></i>
+      </a>
 
-        <a href="https://medium.com/@kolbasarmeryem" className="home__social-link" target="_blank">
-        <i class="fa-brands fa-medium"></i>
-        </a>
+      <a 
+        href="https://medium.com/@kolbasarmeryem" 
+        className="home__social-link" 
+        target="_blank" 
+        rel="noreferrer"
+      >
+        <i className="fa-brands fa-medium"></i>
+      </a>
 
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
